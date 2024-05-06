c9d45d74b69a04e1e026d7d8328155656d29fc92
Caleb Lugo <cnl1211999@gmail.com>
2024-05-06 16:25:20 -0700

    feat: add getters

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,3 +1,8 @@
 function creatTodoItem (title, description, dueDate, priority) {
-    return {title, description, dueDate, priority};
+    const getTitle = () => title;
+    const getDescription = () => description;
+    const getDueDate = () => dueDate;
+    const getPriority = () => priority;
+
+    return { getTitle, getDescription, getDueDate, getPriority };
 }
